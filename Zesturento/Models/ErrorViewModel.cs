669913b0c3c27d8cfc9eb5e8b958b25255359752
muhmm <muhmm@DESKTOP-D2VFDTC>
2024-10-29 11:31:05 +0600

    Add project files.

---
+++ Zesturento/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Zesturento.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
